2b849b61bb3e9962654d68f47daf541166f5feac
guiferrarib <80177720+guiferrarib@users.noreply.github.com>
2021-03-06 20:57:20 -0300

    Update YouTubeConfig.java

--- app/src/main/java/com/ferrariapps/youtubeapp/helper/YouTubeConfig.java
+++ app/src/main/java/com/ferrariapps/youtubeapp/helper/YouTubeConfig.java
@@ -3,7 +3,7 @@ package com.ferrariapps.youtubeapp.helper;
 public class YouTubeConfig {
 
     public static String CANAL_ID = "";
-    public static String CHAVE_YOUTUBE_API = "AIzaSyB6U0xJJfnSAYTI0qQNYzCHF3Pl2OChnew";
+    public static String CHAVE_YOUTUBE_API = "CHAVE_API_YOUTUBE";
     public static String URL_BASE = "https://www.googleapis.com/youtube/v3/";
 
 }
